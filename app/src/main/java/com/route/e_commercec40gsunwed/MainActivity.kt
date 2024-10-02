c8499fa247cfe5a925075d6e330899b78a135d6e
Mohamed Ismail <5mohamedih75@gmail.com>
2024-10-02 19:34:28 +0300

    - Finished ProductsCard and LazyVerticalGrid (UI)

--- app/src/main/java/com/route/e_commercec40gsunwed/MainActivity.kt
+++ app/src/main/java/com/route/e_commercec40gsunwed/MainActivity.kt
@@ -3,45 +3,22 @@ package com.route.e_commercec40gsunwed
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.route.e_commercec40gsunwed.components.ProductsGrid
 import com.route.e_commercec40gsunwed.ui.theme.ECommerceC40GSunWedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ECommerceC40GSunWedTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ProductsGrid(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ECommerceC40GSunWedTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/route/e_commercec40gsunwed/ui/theme/Color.kt
+++ app/src/main/java/com/route/e_commercec40gsunwed/ui/theme/Color.kt
@@ -11,3 +11,6 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val Blue = Color(0xFF004182)
+val NightBlue = Color(0xFF06004F)
+val Heather = Color(0xFFb3c6da)
+val FadedBlue = Color(0xFF668db4)
